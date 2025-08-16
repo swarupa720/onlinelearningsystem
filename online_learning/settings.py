d64ce1810318fac14c327b9ad2d5fcb2ac8616ac
Butterbetter-code <annukb43@gmail.com>
2025-08-16 20:58:05 +0530

    My updates for project features

--- online_learning/settings.py
+++ online_learning/settings.py
@@ -118,7 +118,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.1/howto/static-files/
+
+STATIC_URL = '/static/'
+
+# Add this so Django can find your top-level static/ folder
+import os
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
